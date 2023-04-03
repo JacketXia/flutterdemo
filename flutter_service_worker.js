bc0090c84932740b8dfdc168292fd7dc17fb08d6
xiawenbin <xiawenbin@5eplay.com>
2023-04-03 11:27:29 +0800

    no message

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "9094aacdae789dccd67fa32109ff1a18",
-"index.html": "16db69eb1f567bd1df4fb2fda9c25ab0",
-"/": "16db69eb1f567bd1df4fb2fda9c25ab0",
-"main.dart.js": "f69b28c7a08473fd0a82d6313a1af3ee",
+"index.html": "d78342c559017da5d9f3dba78d6e3237",
+"/": "d78342c559017da5d9f3dba78d6e3237",
+"main.dart.js": "a7d4c9d66799dc9960edfa8150480d59",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
